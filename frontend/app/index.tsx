440241e1f5b8a64eb8028d25f0c297dd9ab2b585
randil-h <51442488+randil-h@users.noreply.github.com>
2024-07-27 23:04:13 +0530

    fixed css

--- frontend/app/index.tsx
+++ frontend/app/index.tsx
@@ -1,22 +1,25 @@
-import { View, Text, ImageBackground } from 'react-native'
-import React from 'react'
+import { View, Text, ImageBackground, TouchableOpacity } from 'react-native';
+import React from 'react';
 import tw from "../tailwind";
 
 import openingImage from "../assets/images/kandy-lake.jpg";
 
 const App = () => {
   return (
-    <View className = "flex-1">
+    <View className="flex-1">
       <ImageBackground
         source={openingImage}
         resizeMode="cover"
-        style={tw`flex-1 justify-center items-center`}>
-          <Text style={tw`"text-white text-2xl font-bold text-center mb-2`}>Sri Lanka</Text>
-          <br></br>
-          <Text style={tw`text-white text-2xl font-bold text-center`}>Wonder of Asia</Text>
-        </ImageBackground>
+        style={tw`flex-1 justify-center items-center`}
+      >
+        <Text style={tw`mt-30 text-white text-2xl font-bold text-center mb-2`}>Sri Lanka</Text>
+        <Text style={tw`mt-30 text-white text-2xl font-bold text-center mb-6`}>Wonder of Asia</Text>
+        <TouchableOpacity style={tw`mt-60 bg-blue-500 px-6 py-3 rounded-full`}>
+          <Text style={tw`text-white text-lg font-semibold`}>Get Started</Text>
+        </TouchableOpacity>
+      </ImageBackground>
     </View>
-  )
-}
+  );
+};
 
 export default App;
