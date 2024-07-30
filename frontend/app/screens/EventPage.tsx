5b0ef314f2edd03f0ca85f09b2820d58bc73a8c1
randil-h <51442488+randil-h@users.noreply.github.com>
2024-07-30 10:23:10 +0530

    made UI components mobile friendly

--- frontend/app/screens/EventPage.tsx
+++ frontend/app/screens/EventPage.tsx
@@ -1,11 +1,28 @@
-import React from 'react';
+import * as React from 'react';
+import { View, Text, TouchableOpacity, StyleSheet } from 'react-native';
 
 const EventPage: React.FC = () => {
-  return (
-    <div>
-      <h1>Events Page</h1>
-    </div>
-  );
+    return (
+        <View style={styles.container}>
+            <Text style={styles.title}>Event Page</Text>
+        </View>
+    );
 };
+
+const styles = StyleSheet.create({
+    container: {
+        flex: 1,
+        justifyContent: 'center',
+        alignItems: 'center',
+    },
+    title: {
+        marginTop: 30,
+        color: 'black',
+        fontSize: 24,
+        fontWeight: 'bold',
+        textAlign: 'center',
+        marginBottom: 2,
+    }
+});
 
 export default EventPage;
--- frontend/app/screens/MapPage.tsx
+++ frontend/app/screens/MapPage.tsx
@@ -1,11 +1,28 @@
-import React from 'react';
+import * as React from 'react';
+import { View, Text, TouchableOpacity, StyleSheet } from 'react-native';
 
 const MapPage: React.FC = () => {
     return (
-        <div>
-            <h1>Map Page</h1>
-        </div>
+        <View style={styles.container}>
+            <Text style={styles.title}>Map Page</Text>
+        </View>
     );
 };
+
+const styles = StyleSheet.create({
+    container: {
+        flex: 1,
+        justifyContent: 'center',
+        alignItems: 'center',
+    },
+    title: {
+        marginTop: 30,
+        color: 'black',
+        fontSize: 24,
+        fontWeight: 'bold',
+        textAlign: 'center',
+        marginBottom: 2,
+    }
+});
 
 export default MapPage;
