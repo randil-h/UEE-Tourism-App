1ab8043c10497a168fb07b6d05691de02049ffc9
Scheffel333 <118644309+Scheffel333@users.noreply.github.com>
2024-08-15 08:34:46 +0530

    new solid icons

--- frontend/assets/icons.js
+++ frontend/assets/icons.js
@@ -1,10 +1,10 @@
-import { AntDesign, Feather } from "@expo/vector-icons";
+import {FontAwesome, FontAwesome6, Ionicons, MaterialCommunityIcons} from "@expo/vector-icons";
 import React from "react";
 
 export const icons = {
-  index: (props)=> <AntDesign name="home" size={26} {...props} />,
-  events: (props)=> <Feather name="music" size={24} {...props} />,
-  route: (props)=> <Feather name="compass" size={26} {...props} />,
-  map: (props)=> <Feather name="map" size={24} {...props} />,
-  profile: (props)=> <AntDesign name="user" size={26} {...props} />,
+  index: (props)=> <FontAwesome name="home" size={24} {...props} />,
+  events: (props)=> <FontAwesome name="music" size={24} {...props} />,
+  route: (props)=> <Ionicons name="compass" size={24} {...props} />,
+  map: (props)=> <FontAwesome6 name="map-location-dot" size={24} {...props} />,
+  profile: (props)=> <MaterialCommunityIcons name="account" size={24} {...props} />,
 }
